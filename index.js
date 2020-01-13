9614c0f3dca0ff1cdbd4a9b3ded77d850075be0c
chonthichayaver <successofnoon@gmail.com>
2020-01-13 21:58:02 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+'use strict'
+const { handleText } = require( "./handleText" );
+
+// start
 const http = require('https');
+const request = require('request');
+
+const accessType = 'Bearer';
+const accessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+const LINE_HEADER = {
+  "Content-Type": "application/json",
+  "Authorization": accessType + accessToken
+}
+
+let LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
+
+const replyMessage = (replyToken, message) => {
+  (message) = Array.isArray(message) ? message : [message];
+  return request.post({
+    uri: `${LINE_MESSAGING_API}/reply`,
+    headers: LINE_HEADER,
+    body: JSON.stringify(handleFlex(
+      replyToken,
+      message.map((message) => ({type: 'message', message}))
+    ))
+  });
+};
+
+
